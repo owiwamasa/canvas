0ae66e31ad9927fdce4f9d75a555b141bdcbfd67
Owen Iwamasa <owiwamasa@gmail.com>
2021-09-06 18:58:23 -0400

    fixed deadline error

--- react-app/src/components/CreateJobModal/CreateJobForm.js
+++ react-app/src/components/CreateJobModal/CreateJobForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import DatePicker from "react-datepicker";
-import Errors from "../Errors"
+// import Errors from "../Errors"
 import { createJob } from '../../store/job';
 import "react-datepicker/dist/react-datepicker.css";
 import './CreateJobForm.css'
@@ -10,12 +10,23 @@ function CreateJobForm({setShowModal, artistId}){
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [dueDate, setDueDate] = useState(new Date())
+    const [errors, setErrors] = useState([])
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user);
 
 
     const createOneJob = async (e) => {
         e.preventDefault()
+        const errs = []
+
+        if (!title) errs.push('Title is required')
+        if (title.length > 200) errs.push('Title must be less than 200 characters')
+        if (!description) errs.push('Description is required')
+        if (description.length > 500) errs.push('Description must be less than 500 characters')
+        if (!dueDate) errs.push('Deadline is required')
+        setErrors(errs)
+
+        if (errs.length === 0) {
         const job = {title, description, dueDate: dueDate?.toString() , userId: user.id, artistId}
 
         const success = await dispatch(createJob(job))
@@ -25,12 +36,18 @@ function CreateJobForm({setShowModal, artistId}){
             setDescription('')
             setDueDate(null)
         }
+      }
     }
 
     return(
         <form onSubmit={createOneJob}>
             <div className='form-header'>Send Work Request</div>
-            <Errors />
+            {/* <Errors /> */}
+            <div className='form-errors'>
+                {errors && errors.map(err => (
+                    <div className='error' key={err}>{err}</div>
+                    ))}
+            </div>
             <div className='form-input work-form-input'>
                 <input
                 type='text'
--- react-app/src/components/EditJobModal/EditJobForm.js
+++ react-app/src/components/EditJobModal/EditJobForm.js
@@ -10,20 +10,37 @@ function EditJobForm({setShowModal, job}){
     const [title, setTitle] = useState(job?.title)
     const [description, setDescription] = useState(job?.description)
     const [dueDate, setDueDate] = useState(new Date())
+    const [errors, setErrors] = useState([])
     const dispatch = useDispatch()
 
     const editOneJob = async (e) => {
         e.preventDefault()
+        const errs = []
+
+        if (!title) errs.push('Title is required')
+        if (title.length > 200) errs.push('Title must be less than 200 characters')
+        if (!description) errs.push('Description is required')
+        if (description.length > 500) errs.push('Description must be less than 500 characters')
+        if (!dueDate) errs.push('Deadline is required')
+        setErrors(errs)
+
+        if (errs.length === 0) {
         const editedJob = {title, description, dueDate: dueDate?.toString() , userId: job.userId, artistId: job.artistId}
         const success = await dispatch(editJob(editedJob, job.id))
         if (success) {
             setShowModal(false)
         }
+      }
     }
     return(
         <form onSubmit={editOneJob}>
             <div className='form-header'>Edit Work Request</div>
-            <Errors />
+            {/* <Errors /> */}
+            <div className='form-errors'>
+                {errors && errors.map(err => (
+                    <div className='error' key={err}>{err}</div>
+                    ))}
+            </div>
             <div className='form-input work-form-input'>
                 <input
                 type='text'
